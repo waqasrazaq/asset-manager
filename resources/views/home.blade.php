6ba03b444c0365ded80b1b5136b22ca466a3b7e5
waqas <waqasrazaq@gmail.com>
2018-07-28 14:16:48 +0400

    View for assets listing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,34 @@
 @include('inc.header')
+<div class="container">
+	<div class="row">
+		<legend>All Assests</legend>
+		<table class="table table-striped table-hover ">
+			<thead>
+				<tr>
+					<th>Label</th>
+					<th>Type</th>
+					<th>Status</th>
+					<th>Owner</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+			<tbody>
+				
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td>
+						<a href="{{ url('') }}" class="label label-primary">Read</a> |
+						<a href="{{ url('') }}" class="label label-success">Update</a> |
+						<a href="{{ url('') }}" class="label label-danger">Delete</a>
+					</td>
+				</tr>
+					
+			</tbody>
+		</table> 
 
-
+	</div>
+</div>
 @include('inc.footer')
